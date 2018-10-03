cae526ce774b7ef8bae5ee525179a8c3f4d97f29
Troy Reeves <troyreeves2@gmail.com>
2018-10-03 09:27:17 -0500

    Previous one was incorrect, This one is funtioning

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using CodingClubTest.Models;
 
-namespace CodingClubTest.Controller
+namespace CodingClubTest.Controllers
 {
     public class HomeController : Controller
     {
