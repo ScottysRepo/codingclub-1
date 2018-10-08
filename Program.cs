4aa82fcb4c9e6d51449389b5b062ef15d330bd95
jivengen <jivengen1@buffs.wtamu.edu>
2018-10-08 01:45:27 -0500

    Seeded Db

--- Program.cs
+++ Program.cs
@@ -7,9 +7,48 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+using CodingClubTest;
+using CodingClubTest.Models;
+
 
 namespace CodingClubTest
 {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<CodingClubDbContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
+/*{
     public class Program
     {
         public static void Main(string[] args)
@@ -21,4 +60,4 @@ namespace CodingClubTest
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
-}
+}*/
